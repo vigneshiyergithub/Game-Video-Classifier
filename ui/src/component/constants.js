d9e34e6097314600de540033fcd6dba400e45469
Vignesh Iyer <vigneshiyer94@gmail.com>
2020-12-02 21:36:55 +0530

    updating tags

--- ui/src/component/constants.js
+++ ui/src/component/constants.js
@@ -18,5 +18,27 @@ export const CATEGORIES = [
   ]
 
 export const TAGS = [
-   'rat', 'killleader', 'jump'
+    'Rat',
+    'bubbleplay',
+    'Killleader',
+    'Shouting',
+    'Kraber',
+    '200 IQ Ults',
+    'Final Ring plays',
+    'Graple plays',
+    'What the fuck',
+    'Squad wipes',
+    '1 v 6',
+    'Clutch',
+    'Lifeline revive plays',
+    'Mastiff',
+    'PK',
+    'Prowler',
+    'Kraber',
+    'G7',
+    'Gold Devotion',
+    'R-99',
+    'R-301',
+    'Flatline',
+    'Wingman'
 ]
